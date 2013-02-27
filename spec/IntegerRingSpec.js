a45a51581ff92c2dd578c260bbaa082575cfb2cb
mf <molefrog@gmail.com>
2013-02-27 02:09:39 +0100

    Migration to Mocha testing library

--- spec/IntegerRingSpec.js
+++ spec/IntegerRingSpec.js
@@ -1,14 +1,20 @@
+if(typeof exports === 'undefined') {
+	rye = window;
+} else {
+ 	rye = require('../lib/rye.js');
+ }
+
 describe('class IntegerRing', function() {
-	var ring = new IntegerRing();
+	var ring = new rye.IntegerRing();
 	
 	it('can be factorized', function() {
-		expect(ring.factorSet(3)).toEqual([0, 1, 2]);
-		expect(function() {ring.factorSet(0)}).toThrow();
+		ring.factorSet(3).should.eql([0, 1, 2]);
+		(function() {ring.factorSet(0)}).should.throw();
 	});
 	
 	it('can divide by a modulo', function() {
-		expect(ring.mod(73, 10)).toEqual(3);
-		expect(ring.mod(0, 243)).toEqual(0);
-		expect(function() { ring.mod(1337, 0); }).toThrow();
+		ring.mod(73, 10).should.eql(3);
+		ring.mod(0, 243).should.eql(0);
+		(function() { ring.mod(1337, 0); }).should.throw();
 	});
 });
